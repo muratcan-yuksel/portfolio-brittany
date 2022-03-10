ae1995895eb023bc8aa515c3dafa2a9c24fa30fc
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-10 04:54:45 +0300

    about page almost done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,11 +7,51 @@ const About = () => {
     <div>
       <Container>
         <Row>
-          <Col xs={12}>
-            <div className="aboutTitle">About me</div>
+          <Col xs={12} md={6}>
+            <div className="aboutText d-flex flex-column">
+              <h4 className="aboutTitle">About Me</h4>
+              <span className="aboutLine"></span>
+              <p className="aboutPara">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Suscipit corporis pariatur voluptas beatae expedita esse eos
+                voluptatem placeat veniam cum, ipsam officia error debitis
+                exercitationem amet sed, dolorem iste voluptatum.
+              </p>{" "}
+              <p className="aboutPara">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Suscipit corporis pariatur voluptas beatae expedita esse eos
+                voluptatem placeat veniam cum, ipsam officia error debitis
+                exercitationem amet sed, dolorem iste voluptatum.
+              </p>{" "}
+              <p className="aboutPara">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Suscipit corporis pariatur voluptas beatae expedita esse eos
+                voluptatem placeat
+              </p>
+              <span className="aboutPara">
+                Here are some of the technologies I currently use:
+              </span>
+              <Row>
+                <Col>
+                  <ul className="techList">
+                    <li>bir</li>
+                    <li>bir</li>
+                    <li>bir</li>
+                  </ul>{" "}
+                </Col>
+                <Col>
+                  <ul className="techList">
+                    <li>bir</li>
+                    <li>bir</li>
+                    <li>bir</li>
+                  </ul>{" "}
+                </Col>{" "}
+              </Row>
+            </div>
           </Col>
-          <Col xs={12}>
-            <div className="aboutLine"></div>
+          {/* image col */}
+          <Col className="d-flex align-items-center" xs={12} md={6}>
+            <div className="pp"></div>
           </Col>
         </Row>
       </Container>
